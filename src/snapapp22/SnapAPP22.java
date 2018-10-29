15a7eaf6be42e75128cd67b22ac90477e92ce26c
donayrsbk <decrout450@gmail.com>
2018-10-29 09:40:55 +0700

    upload pertama

---
+++ src/snapapp22/SnapAPP22.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snapapp22;
+
+/**
+ *
+ * @author Lucidluc
+ */
+public class SnapAPP22 extends SnapFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SnapAPP22 snap = new SnapAPP22();
+            snap.setVisible(true);// TODO code application logic here
+    }
+    
+}
